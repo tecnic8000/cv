be8a1adee5c215d9656de95af4883a75844aa2ad
MHarri Tran <humanhoang@proton.me>
2025-09-30 23:13:51 +0700

    jsonDone-nowFinishCV2

---
+++ cv8001/@getcv.py
@@ -0,0 +1,93 @@
+import json
+import os
+import re
+
+cvDict = {
+      "contact": {}, "education": {}, "interest": {}, "certificate": [],
+      "job": {k: {s: {} for s in ["exp", "obj", "persona", "proj", "skill"]} for k in ["dev", "art", "trade"] }
+    }
+
+def parseContent(dict, sect, data, spec=False):
+  dict[sect]["header"] = data[0].strip()
+  key, lines = None, data[1].strip().splitlines()
+  for line in lines:
+    if line.startswith("[") and line.endswith("]"):
+      key = line.strip("[]")
+      dict[sect][key] = "" if spec else []  # init
+    elif key:
+      dict[sect][key] += line if spec else [line.strip()]
+
+def parseJob(dict, sect):
+    jobpart = re.split(r"###", sect)  # Split the job section into subsections
+    jobSection = [p.strip() for p in jobpart if p.strip()]  # Remove empty parts
+    for js in jobSection:
+        subsect = js.split("?")  # Split into subsection name and content
+        if len(subsect) == 2:  # Ensure there is both a name and content
+            match (subsect[0].split("|", 1)[0].strip()):
+                case "Objective": parseContent(dict, "obj", subsect)
+                case "Experience": parseContent(dict, "exp", subsect, True)
+                case "Persona": parseContent(dict, "persona", subsect)
+                case "Project": parseContent(dict, "proj", subsect)
+                case "Skill": parseContent(dict, "skill", subsect, True)
+                case _: print(f"Unrecognized subsection: {subsect[0]}")
+        else:
+            print(f"Malformed subsection: {js}")
+
+def getCV(filepath, start_marker, end_marker, jsonpath):
+    capture = False
+    lines = []
+
+    with open(filepath, "r", encoding="utf-8") as f:
+        for line in f:
+            if start_marker in line:
+                capture = True
+                continue
+            if end_marker in line:
+                break
+            if capture:
+                lines.append(line)
+    capturedString = "".join(lines).strip() # first read as string
+
+    parts = re.split(r"(?<!#)##(?!#)", capturedString) # find everything between ## ... until the next ##
+    sectionlist = [p.strip() for p in parts if p.strip()]
+   
+    for s in sectionlist:
+        section = s.split("$")
+        if len(section) == 2 :
+            match (section[0].split("|", 1)[0].strip()):
+              case "Contact": parseContent(cvDict, "contact", section)
+              case "Education": parseContent(cvDict, "education", section)
+              case "Interest": parseContent(cvDict, "interest", section)
+              case "Certificate": 
+                cvDict["certificate"].append(section[0].strip())
+                cvDict["certificate"].append(section[1].strip())
+              case "DEV": parseJob(cvDict["job"]["dev"], section[1])
+              case "ART":  parseJob(cvDict["job"]["art"], section[1])
+              case "TRADE": parseJob(cvDict["job"]["trade"], section[1])
+              case _: print("default case hit")
+          
+    # Load existing JSON data if the file exists
+    if os.path.exists(jsonpath):
+        with open(jsonpath, "r", encoding="utf-8") as f:
+            try:
+                existingCvDict = json.load(f)
+            except json.JSONDecodeError:
+                existingCvDict = {}
+    else:
+        existingCvDict = {}
+    
+    existingCvDict.update(cvDict)
+        
+    with open (jsonpath, "w") as f:
+        json.dump(existingCvDict, f, indent=3, ensure_ascii=False) # dict to json
+    print("\n cv updated @ ~/REPO1/cv/cv8001/cv.json \n")
+    return existingCvDict
+
+getCV(
+    os.path.expanduser("~/REPO1/note1.md"),
+    "[CV-START]",
+    "[CV-END]",
+    os.path.expanduser("~/REPO1/cv/cv8001/src/cv.json")
+)
+
+# print(getCV)
--- cv8001/src/App.tsx
+++ cv8001/src/App.tsx
@@ -6,23 +6,10 @@ import { Card, CardHeader, CardDescription, CardFooter } from "@/components/ui/c
 // import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 // import { motion } from "motion/react";
 import { useEffect, useState } from "react";
+import cv2 from "./cv.json"
 // import * as THREE from "three";
 // import { Canvas, useFrame } from "@react-three/fiber";
 // import { useGLTF, OrbitControls, Environment } from "@react-three/drei";
-
-interface CV {
-  Contact: { [key: string]: string };
-  Education: { [key: string]: string };
-  Certificate: { [key: string]: string };
-  Interest: { [key: string]: string };
-  Job: {
-    [jobType: string]: {
-      [sectionType: string]: {
-        [key: string]: string
-      };
-    }
-  }
-}
 
 // function Model({ url }: { url: string }) {
 //   const { scene, animations } = useGLTF(url);
@@ -70,14 +57,28 @@ interface CV {
 //     </div>
 //   );
 // }
+interface CV {
+    Contact: { [key: string]: string };
+    Education: { [key: string]: string };
+    Certificate: { [key: string]: string };
+    Interest: { [key: string]: string };
+    Job: {
+      [jobType: string]: {
+        [sectionType: string]: {
+          [key: string]: string
+        };
+      }
+    }
+  }
+function CV1() {
+  
 
-function CVblock() {
   const [cv, setCV] = useState<CV | null>(null);
   // const [langMode, setLangMode] = useState<"vn" | "en" | "fr" | "jp">("vn");
-  const langMode:string = "vn";
+  const langMode: string = "vn";
   // const [langIndex, setLangIndex] = useState<number>(0);
   // const [jobMode, setJobMode] = useState<"dev" | "art" | "trade">("dev");
-  const jobMode:string = "dev";
+  const jobMode: string = "dev";
 
   let langIndex: number = 0;
   useEffect(() => {
@@ -96,14 +97,11 @@ function CVblock() {
     case "fr": langIndex = 2; break;
     case "jp": langIndex = 3; break;
   }
-  // console.log(langMode, langParam, titleIndex);
-  if (!cv) return <div>LOADING...</div>
-  // console.log(langIndex, langMode)
 
-  // console.log(cv.Contact["__name__"].split(/\n/).slice(1, -1)[1])
+  if (!cv) return <div>LOADING... goBackend seems offline</div>
+  // console.log(langIndex, langMode)
   // console.log(cv.Job[jobMode].exp["vn"])
   // console.log(cv.Job[jobMode].proj["vn"])
-
   return (<>
 
     <Card className="bg-white m-4 p-2">
@@ -208,7 +206,7 @@ function CVblock() {
       <div className="border-t border-gray-900 w-fit">
         <CardFooter className="pt-2">{cv.Education["header"].split("@")[0].split("|")[langIndex]}</CardFooter>
         <CardFooter>
-          {cv.Education["header"].split("@")[1].split("_")[0]}<br/>
+          {cv.Education["header"].split("@")[1].split("_")[0]}<br />
           {cv.Education["header"].split("@")[1].split("_")[1]}&nbsp;
           {cv.Education["header"].split("@")[1].split("_")[2]}
         </CardFooter>
@@ -227,12 +225,159 @@ function CVblock() {
   </>
   )
 }
+
+function CV2() {
+  
+  console.log(cv2);
+
+  const [cv, setCV] = useState<CV | null>(null);
+  // const [langMode, setLangMode] = useState<"vn" | "en" | "fr" | "jp">("vn");
+  const langMode: string = "vn";
+  // const [langIndex, setLangIndex] = useState<number>(0);
+  // const [jobMode, setJobMode] = useState<"dev" | "art" | "trade">("dev");
+  const jobMode: string = "dev";
+
+  let langIndex: number = 0;
+  switch (langMode) {
+    case "vn": langIndex = 1; break;
+    case "fr": langIndex = 2; break;
+    case "jp": langIndex = 3; break;
+  }
+
+  if (!cv2) return <div>LOADING JSON</div>
+  // console.log(langIndex, langMode)
+  // console.log(cv.Job[jobMode].exp["vn"])
+  // console.log(cv.Job[jobMode].proj["vn"])
+  return (<>
+
+    <Card className="bg-white m-4 p-2">
+
+      {/* PROFILE */}
+      {/* <div className="flex ">
+        <CardHeader className="bg-blue-300 w-100 m-1 border-r-2 rounded-l-md" >
+          <div className="h-1 pt-2 text-2xl">{cv2.Contact["__name__"].split(/\n/).slice(1, -1)[langIndex]}</div><br />Fullstack Developer
+        </CardHeader>
+        <div>
+          <CardDescription className="bg-teal-000">
+            {((item) => {
+              switch (jobMode) {
+                case "dev": return (
+                  <div>
+                    <span className="text-lg">{item[0]}</span><br />
+                    {item[1]}<br />{item[2]}<br />
+                  </div>);
+              }
+            })(cv.Contact["__link__"].split("\n").slice(1, -1))
+            }
+
+          </CardDescription>
+          <CardDescription className="bg-pink-000">
+            {cv.Contact["__address__"].split(/\n/).slice(1, -1)[langIndex].slice(10)}
+          </CardDescription>
+        </div>
+      </div> */}
+
+      {/* <CardDescription className="">{cv.Job[jobMode].obj[langMode]}</CardDescription> */}
+
+      {/* EXPERIENCE */}
+      {/* <div className="bg-blue-000">
+        <CardFooter className="border-t border-l ml-2 border-gray-950">{cv.Job[jobMode].exp["header"].split("|")[langIndex]}</CardFooter>
+        <CardDescription >
+          {cv.Job[jobMode].exp[langMode].split("@").map((item, index) => (
+            <div key={index}> {item.split("__").slice(1).map((detail, i) => {
+              switch (i) {
+                case 0: return (
+                  <div key={i} className="bg-blue-300 max-w-2xl px-3 py-1 mb-1 flex justify-between rounded-sm">
+                    <span >
+                      {detail.split("_")[0]}&nbsp;- {detail.split("_")[1]}
+                    </span>
+                    <span>
+                      {detail.split("_")[2]}&nbsp;{detail.split("_")[3]}
+                    </span>
+                  </div>);
+                case 1: return detail.split("\n").slice(1).map((sub, i) => (<div key={i}>{sub}</div>));
+                default: return null;
+              }
+            })} </div>
+          ))
+          }
+        </CardDescription>
+      </div> */}
+      {/* <CardDescription>{cv.Job[jobMode].skill["__backend__"]}</CardDescription> */}
+
+      {/* SKILL */}
+      {/* <div >
+        <CardFooter className="border w-fit pt-1 mb-2 rounded-md">{cv.Job[jobMode].skill["header"].split("|")[langIndex]}</CardFooter>
+        <CardDescription >
+          {
+            (() => {
+              switch (jobMode) {
+                case "dev": return (
+                  <div>
+                    &middot;{cv.Job[jobMode].skill["__Language__"]}<br />
+                    &middot;{cv.Job[jobMode].skill["__Frontend__"]}<br />
+                    &middot;{cv.Job[jobMode].skill["__Backend__"]}<br />
+                    &middot;{cv.Job[jobMode].skill["__Database__"]}
+                  </div>
+                )
+                case "art": return "empty.."
+                case "trade": return "empty.."
+                default: return null
+              }
+            })
+              ()}
+        </CardDescription>
+      </div> */}
+
+
+      {/* PROJECT */}
+      {/* <div className="bg-blue-000">
+        <CardFooter className="border-1 w-fit pt-1 mb-2 rounded-lg">{cv.Job[jobMode].proj["header"].split("|")[langIndex]}</CardFooter>
+        <CardDescription>
+          {cv.Job[jobMode].proj[langMode].split("\n").slice(1, -1).map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+        </CardDescription>
+      </div> */}
+      {/* PERSONA */}
+      <CardDescription>{cv2.job[jobMode].persona[langMode]}</CardDescription>
+
+      {/* CERTIFICATE */}
+      {/* <div className="bg-green-300">
+        <CardHeader>{cv.Certificate[0].split("|")[langIndex]}</CardHeader>
+        <CardDescription>{cv.Certificate[1]}</CardDescription>
+      </div> */}
+
+      {/* EDUCATION */}
+      {/* <div className="border-t border-gray-900 w-fit">
+        <CardFooter className="pt-2">{cv.Education["header"].split("@")[0].split("|")[langIndex]}</CardFooter>
+        <CardFooter>
+          {cv.Education["header"].split("@")[1].split("_")[0]}<br />
+          {cv.Education["header"].split("@")[1].split("_")[1]}&nbsp;
+          {cv.Education["header"].split("@")[1].split("_")[2]}
+        </CardFooter>
+        <CardDescription>
+          {cv.Education[langMode].split("\n").slice(1, -1).map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+        </CardDescription>
+      </div> */}
+
+      <div className="">
+        {/* v0.1 */}
+        <div className="bg-blue-400 w-[10px] h-[10px] flex float-end"></div>
+      </div>
+    </Card>
+  </>
+  )
+}
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CVblock />} />
+        <Route path="/" element={<CV2 />} />
+        <Route path="/v1" element={<CV1 />} />
       </Routes>
     </BrowserRouter>
   )
