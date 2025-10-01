a9704cb01772b59d9c0918d7d940b5a9a40d3113
MHarri Tran <humanhoang@proton.me>
2025-10-01 15:50:57 +0700

    centerdiv1

--- cv8001/src/App.tsx
+++ cv8001/src/App.tsx
@@ -258,135 +258,136 @@ function CV2() {
   // console.log(cv.Job[jobMode].exp["vn"])
   // console.log(cv.Job[jobMode].proj["vn"])
   return (<>
+    <div className="flex items-center justify-center" >
+      <Card className="bg-white m-4 p-2 max-w-2xl">
 
-    <Card className="bg-white m-4 p-2">
-
-      {/* PROFILE */}
-      <div className="flex ">
-        <CardHeader className="bg-blue-300 w-100 m-1 border-r-2 rounded-l-md" >
-          <div className="h-1 pt-2 text-2xl">{cv.contact["name"][langIndex]}</div><br />Frontend Developer
-        </CardHeader>
-        <div>
-          <CardDescription className="bg-teal-000">
-            {((item) => {
-              switch (jobMode) {
-                case "dev": return (
-                  <div>
-                    <span className="text-lg">{item[0]}</span><br />
-                    {item[1]}<br />{item[2]}<br />
-                  </div>);
+        {/* PROFILE */}
+        <div className="flex ">
+          <CardHeader className="bg-blue-300 w-100 m-1 border-r-2 rounded-l-md" >
+            <div className="h-1 pt-2 text-2xl">{cv.contact["name"][langIndex]}</div><br />Frontend Developer
+          </CardHeader>
+          <div>
+            <CardDescription className="bg-teal-000">
+              {((item) => {
+                switch (jobMode) {
+                  case "dev": return (
+                    <div>
+                      <span className="text-lg">{item[0]}</span><br />
+                      {item[1]}<br />{item[2]}<br />
+                    </div>);
+                }
+              })(cv.contact["link"])
               }
-            })(cv.contact["link"])
-            }
 
-          </CardDescription>
-          <CardDescription className="bg-pink-000">
-            {cv.contact["address"][langIndex].slice(10)}
+            </CardDescription>
+            <CardDescription className="bg-pink-000">
+              {cv.contact["address"][langIndex].slice(10)}
+            </CardDescription>
+          </div>
+        </div>
+
+        {/* <CardDescription className="">{cv.Job[jobMode].obj[langMode]}</CardDescription> */}
+
+        {/* EXPERIENCE */}
+        <div className="bg-blue-000">
+          <CardFooter className="border-t border-l ml-2 rounded-tl-lg border-gray-950">
+            {(cv.job[jobMode].exp["header"] as string).split("|")[langIndex]}
+          </CardFooter>
+          <CardDescription >
+            {(cv.job[jobMode].exp[langMode] as string).split("@").map((item, index) => (
+              <div key={index}>{item.split("__").slice(1).map((detail, i) => {
+                switch (i) {
+                  case 0: return (
+                    <div key={i} className="bg-blue-300 max-w-2xl px-3 py-1 mb-1 flex justify-between rounded-sm">
+                      <span >
+                        {detail.split("_")[0]}&nbsp;- {detail.split("_")[1]}
+                      </span>
+                      <span>
+                        {detail.split("_")[2]}&nbsp;{detail.split("_")[3]}
+                      </span>
+                    </div>
+                  )
+                  case 1: return detail.split("-").slice(1).map((sub, i) => (<div key={i}>- {sub}</div>));
+                  default: return null;
+                }
+              })}
+              </div>
+            ))}
           </CardDescription>
         </div>
-      </div>
+        {/* <CardDescription>{cv.Job[jobMode].skill["__backend__"]}</CardDescription> */}
 
-      {/* <CardDescription className="">{cv.Job[jobMode].obj[langMode]}</CardDescription> */}
+        {/* SKILL & PERSONA */}
+        <div >
+          <CardFooter className="border w-fit pt-1 mb-2 rounded-md">
+            {(cv.job[jobMode].skill["header"] as string).split("|")[langIndex]}
+          </CardFooter>
+          <CardDescription >
+            {
+              (() => {
+                switch (jobMode) {
+                  case "dev": return (
+                    <div>
+                      &middot; {cv.job[jobMode].skill["Language"]}<br />
+                      &middot; {cv.job[jobMode].skill["Frontend"]}<br />
+                      &middot; {cv.job[jobMode].skill["Backend"]}<br />
+                      &middot; {cv.job[jobMode].skill["Database"]}
+                    </div>
+                  )
+                  case "art": return "empty.."
+                  case "trade": return "empty.."
+                  default: return null
+                }
+              })
+                ()}
+            {/* PERSONA */}
+            {(cv.job[jobMode].persona[langMode] as string[]).map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </CardDescription>
+        </div>
 
-      {/* EXPERIENCE */}
-      <div className="bg-blue-000">
-        <CardFooter className="border-t border-l ml-2 rounded-tl-lg border-gray-950">
-          {(cv.job[jobMode].exp["header"] as string).split("|")[langIndex]}
-        </CardFooter>
-        <CardDescription >
-          {(cv.job[jobMode].exp[langMode] as string).split("@").map((item, index) => (
-            <div key={index}>{item.split("__").slice(1).map((detail, i) => {
-              switch (i) {
-                case 0: return (
-                  <div key={i} className="bg-blue-300 max-w-2xl px-3 py-1 mb-1 flex justify-between rounded-sm">
-                    <span >
-                      {detail.split("_")[0]}&nbsp;- {detail.split("_")[1]}
-                    </span>
-                    <span>
-                      {detail.split("_")[2]}&nbsp;{detail.split("_")[3]}
-                    </span>
-                  </div>
-                )
-                case 1: return detail.split("-").slice(1).map((sub, i) => (<div key={i}>- {sub}</div>));
-                default: return null;
-              }
-            })}
-            </div>
-          ))}
-        </CardDescription>
-      </div>
-      {/* <CardDescription>{cv.Job[jobMode].skill["__backend__"]}</CardDescription> */}
+        {/* PROJECT */}
+        <div className="bg-blue-000">
+          <CardFooter className="border-1 w-fit pt-1 mb-2 rounded-lg">
+            {(cv.job[jobMode].proj["header"] as string).split("|")[langIndex]}
+          </CardFooter>
+          <CardDescription>
+            {(cv.job[jobMode].proj[langMode] as string[]).map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </CardDescription>
+        </div>
 
-      {/* SKILL & PERSONA */}
-      <div >
-        <CardFooter className="border w-fit pt-1 mb-2 rounded-md">
-          {(cv.job[jobMode].skill["header"] as string).split("|")[langIndex]}
-        </CardFooter>
-        <CardDescription >
-          {
-            (() => {
-              switch (jobMode) {
-                case "dev": return (
-                  <div>
-                    &middot; {cv.job[jobMode].skill["Language"]}<br />
-                    &middot; {cv.job[jobMode].skill["Frontend"]}<br />
-                    &middot; {cv.job[jobMode].skill["Backend"]}<br />
-                    &middot; {cv.job[jobMode].skill["Database"]}
-                  </div>
-                )
-                case "art": return "empty.."
-                case "trade": return "empty.."
-                default: return null
-              }
-            })
-              ()}
-          {/* PERSONA */}
-          {(cv.job[jobMode].persona[langMode] as string[]).map((item, index) => (
-            <div key={index}>{item}</div>
-          ))}
-        </CardDescription>
-      </div>
-
-      {/* PROJECT */}
-      <div className="bg-blue-000">
-        <CardFooter className="border-1 w-fit pt-1 mb-2 rounded-lg">
-          {(cv.job[jobMode].proj["header"] as string).split("|")[langIndex]}
-        </CardFooter>
-        <CardDescription>
-          {(cv.job[jobMode].proj[langMode] as string[]).map((item, index) => (
-            <div key={index}>{item}</div>
-          ))}
-        </CardDescription>
-      </div>
-
-      {/* CERTIFICATE */}
-      {/* <div className="bg-green-300">
+        {/* CERTIFICATE */}
+        {/* <div className="bg-green-300">
         <CardHeader>{cv.Certificate[0].split("|")[langIndex]}</CardHeader>
         <CardDescription>{cv.Certificate[1]}</CardDescription>
       </div> */}
 
-      {/* EDUCATION */}
-      <div className="">
-        <CardFooter className="pt-0 border-t border-r rounded-tr-lg border-gray-900 w-fit">
-          {(cv.education["header"] as string).split("|")[langIndex]}
-        </CardFooter>
-        <CardFooter>
-          {(cv.education["header"] as string).split("@")[1].split("_")[0]}<br />
-          {(cv.education["header"] as string).split("@")[1].split("_")[1]}&nbsp;
-          {(cv.education["header"] as string).split("@")[1].split("_")[2]}
-        </CardFooter>
-        <CardDescription>
-          {(cv.education[langMode] as string[]).map((item, index) => (
-            <div key={index}>{item}</div>
-          ))}
-        </CardDescription>
-      </div>
+        {/* EDUCATION */}
+        <div className="">
+          <CardFooter className="pt-0 border-t border-r rounded-tr-lg border-gray-900 w-fit">
+            {(cv.education["header"] as string).split("|")[langIndex]}
+          </CardFooter>
+          <CardFooter>
+            {(cv.education["header"] as string).split("@")[1].split("_")[0]}<br />
+            {(cv.education["header"] as string).split("@")[1].split("_")[1]}&nbsp;
+            {(cv.education["header"] as string).split("@")[1].split("_")[2]}
+          </CardFooter>
+          <CardDescription>
+            {(cv.education[langMode] as string[]).map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </CardDescription>
+        </div>
 
-      <div className="">
-        {/* v0.1 */}
-        <div className="bg-blue-400 w-[10px] h-[10px] flex float-end"></div>
-      </div>
-    </Card>
+        <div className="">
+          {/* v0.1 */}
+          <div className="bg-blue-400 w-[10px] h-[10px] flex float-end"></div>
+        </div>
+      </Card>
+    </div>
   </>
   )
 }
